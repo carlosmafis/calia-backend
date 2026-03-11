3182259f9f918ef0da491388b32cebce0c01076f
Carlos Adriano Lourenço da Silva <carlosad16@hotmail.com>
2026-03-11 17:26:21 -0300

    Create config.py

---
+++ core/config.py
@@ -0,0 +1,10 @@
+import os
+from supabase import create_client
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_SERVICE_KEY = os.environ.get("SUPABASE_SERVICE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_SERVICE_KEY)
