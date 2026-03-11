82974c3a709ad37184debf6fb119d22718758a9b
Carlos Adriano Lourenço da Silva <carlosad16@hotmail.com>
2026-03-11 17:25:46 -0300

    Create auth.py

---
+++ core/auth.py
@@ -0,0 +1,25 @@
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from core.config import supabase
+
+security = HTTPBearer()
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+
+    token = credentials.credentials
+
+    try:
+        user_response = supabase.auth.get_user(token)
+        user_data = user_response.user
+    except:
+        raise HTTPException(status_code=401)
+
+    profile = supabase.table("profiles") \
+        .select("*") \
+        .eq("id", user_data.id) \
+        .execute()
+
+    if not profile.data:
+        raise HTTPException(status_code=403)
+
+    return profile.data[0]
