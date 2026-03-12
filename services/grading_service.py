5efda3480a1a7af6799fdef8d4e43b7b1769eb6d
Carlos Adriano Lourenço da Silva <carlosad16@hotmail.com>
2026-03-11 21:51:52 -0300

    Update grading_service.py

--- services/grading_service.py
+++ services/grading_service.py
@@ -2,13 +2,9 @@ def calculate_score(student_answers, correct_answers):
 
     score = 0
 
-    for q in correct_answers:
+    for r, g in zip(student_answers, correct_answers):
 
-        number = str(q["question_number"])
-        correct = q["correct_answer"]
-        weight = q["weight"]
-
-        if student_answers.get(number) == correct:
-            score += weight
+        if r == g:
+            score += 1
 
     return score
